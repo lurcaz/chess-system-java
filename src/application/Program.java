de1d77ffe9da7d5d547edff82476dcdf62994999
Lucas <lucas.desa@outlook.com.br>
2022-12-08 14:22:20 -0300

    Possibles moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,9 @@ public class Program {
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
+				boolean [][] possibleMoves = chessMath.possibleMovies(source);
+				UI.clearScreen();
+				UI.printBoard(chessMath.getPiece(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
